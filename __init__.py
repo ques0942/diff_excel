185e430e2d44bb8b0a801f6c8b5f678f558dec71
ques0942 <nari0942@gmail.com>
2013-03-13 20:45:05 +0900

    xlsxからcsvを生成する機能を追加

--- __init__.py
+++ __init__.py
@@ -42,24 +42,49 @@ def diff_ws(ws1, ws2):
     for row in dim1:
         tmp = []
         for col in row:
-            tmp.append(col.value)
+            tmp.append((col.value))
         csv1.append(tuple(tmp))
     for row in dim2:
         tmp = []
         for col in row:
-            tmp.append(col.value)
+            tmp.append((col.value))
         csv2.append(tuple(tmp))
 
     d = difflib.Differ()
     return list(d.compare(csv1, csv2))
-    
+
+def convert_csv(book_name, num=0):
+    """
+    xlsxのシートのうち1つをcsvに変換する．
+    デフォルトでは，先頭のシートを変換する．
+    """
+    wb = xl.load_workbook(book_name)
+    sheet_names = wb.get_sheet_names()
+    target_name = sheet_names[num]
+    ws = wb.get_sheet_by_name(target_name)
+    dim = ws.calculate_dimension()
+    dim = ws.range(dim)
+    csv = []
+    for row in dim:
+        tmp = []
+        for col in row:
+            tmp.append(str(col.value))
+        csv.append(tmp)
+    return csv
 
 if __name__ == '__main__':
     import sys
     from pprint import pprint
-    book_name1 = sys.argv[1]
-    book_name2 = sys.argv[2]
-    wb1 = xl.load_workbook(book_name1)
-    wb2 = xl.load_workbook(book_name2)
-    diff_sheets, result_diff = diff_wb(wb1, wb2)
-    pprint(result_diff)
+    if len(sys.argv) < 2:
+        print 'no input'
+        sys.exit(-1)
+    elif len(sys.argv) == 2:
+        for row in convert_csv(sys.argv[1]):
+            print ','.join(row)
+    else:
+        book_name1 = sys.argv[1]
+        book_name2 = sys.argv[2]
+        wb1 = xl.load_workbook(book_name1)
+        wb2 = xl.load_workbook(book_name2)
+        diff_sheets, result_diff = diff_wb(wb1, wb2)
+        pprint(result_diff)
